ecf34ae8965ea317675ea9a1d18f5317c39763a9
Anug Saha <anugsaha@gmail.com>
2022-07-26 12:26:58 -0400

    use keyword volatile on total

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // your code here
+        SavingsAccount anugAccount = new SavingsAccount();
+
+        Thread t1 = new Thread(()-> anugAccount.deposit(1_000));
+        Thread t2 = new Thread(()-> anugAccount.deposit(3_000));
+        Thread t3 = new Thread(()-> anugAccount.withdraw(200));
+        Thread t4 = new Thread(()-> anugAccount.withdraw(600));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
+
+        System.out.println("Total in account: " + anugAccount.getTotal());
     }
 }
--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,13 +1,20 @@
 public class SavingsAccount {
 
-  private long total = 0;
+  private volatile long total = 0;
 
   public boolean withdraw(long amount) {
-      
+    if(total > amount) {
+      total -= amount;
+      return true;
+    }else{
+      System.out.println("not enough in account to withdraw");
+      System.out.println("Currently in account: " + getTotal());
+      return false;
+    }
   }
 
   public void deposit(long amount) {
-
+    total += amount;
   }
 
   public long getTotal() {
